5f36a4911629dfff0b8bd05de4e750321b771462
Chang Hu <changhu@illinois.edu>
2018-12-04 17:07:55 -0600

    - Change scrpit to function.

--- gene_dict.py
+++ gene_dict.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-hugo_symbol = pd.read_csv('data/Hugo_Symbol.csv').dropna()
-hugo_symbol.PyClonePhyloCluster = hugo_symbol.PyClonePhyloCluster.astype(int)
-hugo_symbol['combined'] = hugo_symbol.apply(lambda x: (x.SampleID, x.Hugo_Symbol), axis=1)
-mapping = dict(zip(hugo_symbol.combined, hugo_symbol['PyClonePhyloCluster']))
-gene_list = set(hugo_symbol['Hugo_Symbol'])
+def load_dict():
+	hugo_symbol = pd.read_csv('data/Hugo_Symbol.csv').dropna()
+	hugo_symbol.PyClonePhyloCluster = hugo_symbol.PyClonePhyloCluster.astype(int)
+	hugo_symbol['combined'] = hugo_symbol.apply(lambda x: (x.SampleID, x.Hugo_Symbol), axis=1)
+	mapping = dict(zip(hugo_symbol.combined, hugo_symbol['PyClonePhyloCluster']))
+	gene_list = set(hugo_symbol['Hugo_Symbol'])
+	return mapping, gene_list
